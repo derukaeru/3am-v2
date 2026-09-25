71dd193ce14d484d39dd0c9e7c0da3354675e737
dalifyyy <d4lified@gmail.com>
2026-09-25 12:56:59 +0800

    feat: cleaned some files and added templates for player and map

--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -3,10 +3,13 @@ class_name Player extends CharacterBody2D
 const SPEED = 120.0
 
 func _physics_process(_delta: float) -> void:
-	var direction = Input.get_vector("left", "right", "forward", "backward")
+	
+	
+	var direction = Input.get_axis("left", "right")
 	if direction:
+		direction = direction.normalized()
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 0.4)
 	
 	move_and_slide()
